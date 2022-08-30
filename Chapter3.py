64d669c919a72361eadda8b5710739587c769470
Lucas <90351520+Raven-Chamber-Coding@users.noreply.github.com>
2022-08-30 08:00:55 -0500

    Add files via upload

---
+++ Chapter3.py
@@ -0,0 +1,10 @@
+"""
+Week 1 Programming Assignment
+Lucas Landis
+"""
+
+seconds_per_hour = 60 * 60
+seconds_per_day = seconds_per_hour * 24
+float_answer = seconds_per_day / seconds_per_hour
+integer_answer = seconds_per_day // seconds_per_hour
+print ("Float:", float_answer, "Integer:", integer_answer)
